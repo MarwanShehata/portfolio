95bb873d602e2d94147e085b1ea38a966ee86025
Marwan Shehata <Marwanzeitoun1@gmail.com>
2025-06-30 21:19:12 +0300

    minor modifications

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,44 +8,55 @@ import { Suspense } from 'react'
 import { ContactForm } from './components/form'
 
 const title = 'Contact'
-const description = "Let's chat."
+const description = "Let's chat:"
 
 export const metadata: Metadata = createMetadata({
-	title,
-	description,
-	ogText: 'Want to chat about something? Get in touch.',
+  title,
+  description,
+  ogText: 'Want to chat about something? Get in touch.',
 })
 
 const Contact = () => {
-	try {
-		return (
-			<ReCaptchaProvider reCaptchaKey={env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}>
-				<Section className='gap-0'>
-					<h1>{title}</h1>
-					<p className='text-foreground-lighter'>{description}</p>
-					<Link
-						href='mailto:MarwanShehata@outlook.com'
-						className='text-foreground-lighter underline hover:text-foreground'
-						aria-label='Contact ME!'
-						target='_blank'
-						rel='noopener noreferrer'
-						title='Contact ME!'
-					>
-						MarwanShehata@outlook.com
-					</Link>
-				</Section>
-				<Section delay={0.2}>
-					<Suspense fallback={null}>
-						{/* When you are ready, you will replace this div with the actual form */}
-						{/* <div>Contact form coming soon...</div> */}
-						<ContactForm />
-					</Suspense>
-				</Section>
-			</ReCaptchaProvider>
-		)
-	} catch (_error) {
-		return <div>Error loading contact page.</div>
-	}
+  try {
+    return (
+      <ReCaptchaProvider reCaptchaKey={env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}>
+        <Section className='gap-0'>
+          <h1>{title}</h1>
+          <p className='text-foreground-lighter'>{description}</p>
+          <Link
+            href='mailto:MarwanShehata@outlook.com'
+            className='contents text-foreground-lighter underline hover:text-foreground'
+            aria-label='Contact ME!'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Contact ME!'
+          >
+            MarwanShehata@outlook.com
+          </Link>{' '}
+          or call me on{' '}
+          <Link
+            href='tel:+201121255131'
+            className='contents text-foreground-lighter underline hover:text-foreground'
+            aria-label='Call me on +201121255131'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Call me on +201121255131'
+          >
+            +201121255131
+          </Link>
+        </Section>
+        <Section delay={0.2}>
+          <Suspense fallback={null}>
+            {/* When you are ready, you will replace this div with the actual form */}
+            {/* <div>Contact form coming soon...</div> */}
+            <ContactForm />
+          </Suspense>
+        </Section>
+      </ReCaptchaProvider>
+    )
+  } catch (_error) {
+    return <div>Error loading contact page.</div>
+  }
 }
 
 export default Contact
--- components/footer.tsx
+++ components/footer.tsx
@@ -2,26 +2,29 @@ import { social } from '@/lib/social'
 import { Link } from './link'
 
 export const Footer = () => (
-	<footer className='text-foreground-lighter text-sm leading-relaxed'>
-		<p>
-			&copy; {new Date().getFullYear()} Marwan Shehata. All rights reserved.
-		</p>
-		<p>
-			View the{' '}
-			<Link href={social.github.href} aria-label='View source code on GitHub'>
-				source code
-			</Link>
-			.
-		</p>
-		<p>
-			Visit my linkedin{' '}
-			<Link
-				href='https://www.linkedin.com/in/marwanshehata/'
-				aria-label='Visit my linkedin profile'
-			>
-				@MarwanShehata
-			</Link>
-			.
-		</p>
-	</footer>
+  <footer className='text-foreground-lighter text-sm leading-relaxed'>
+    <p>
+      &copy; {new Date().getFullYear()} Marwan Shehata. All rights reserved.
+    </p>
+    <p>
+      View the{' '}
+      <Link href={social.github.href} aria-label='View source code on GitHub'>
+        source code
+      </Link>
+      .
+    </p>
+    <p>
+      Visit my linkedin{' '}
+      <Link
+        href='https://www.linkedin.com/in/marwanshehata/'
+        aria-label='Visit my linkedin profile'
+      >
+        @MarwanShehata
+      </Link>{', '}
+      or call me on{' '}
+      <Link href='tel:+201121255131' aria-label='Call me on +201121255131'>
+        +201121255131
+      </Link>
+    </p>
+  </footer>
 )
